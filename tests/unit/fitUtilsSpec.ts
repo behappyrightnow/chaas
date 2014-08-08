e079748354b58ad89dae58bcd7dbdb28b12d9998
Somik Raha <somikr@gmail.com>
2014-08-07 20:30:30 -0700

    all tests passing

--- tests/unit/fitUtilsSpec.ts
+++ tests/unit/fitUtilsSpec.ts
@@ -8,11 +8,9 @@ describe('FitUtils', function () {
        fitUtils = new FitUtils();
     });
     it("should camel case Class properly", function() {
-        console.log(fitUtils.camelCaseClass("should i buy milk"));
         expect(fitUtils.camelCaseClass("should i buy milk")).toBe("ShouldIBuyMilk");
     });
     it("should camel case properly", function() {
-        console.log(fitUtils.camelCase("cash in wallet"));
         expect(fitUtils.camelCase("cash in wallet")).toBe("cashInWallet");
     });
     describe("Wiki Element Creation", function() {
@@ -20,13 +18,20 @@ describe('FitUtils', function () {
             var listOfStrings = ["Hello", "World", "Again"];
             var wikiElements:Array<WikiElement> = fitUtils.wikiData(listOfStrings);
             expect(wikiElements).not.toBe(undefined);
-            console.log("Number of elements "+wikiElements.length);
             for (var i=0;i<wikiElements.length;i++) {
-                console.log(wikiElements[i].type);
                 expect(wikiElements[i]).not.toBe(undefined);
                 expect(wikiElements[i].type).toBe("DEFAULT");
             }
-        })
+        });
+        it("should turn piped strings into Table Elements", function () {
+            var listOfStrings = ["|Hello|World|", "|This Is |A Table|"];
+            var wikiElements = fitUtils.wikiData(listOfStrings);
+            expect(wikiElements).not.toBe(undefined);
+            for (var i = 0; i < wikiElements.length; i++) {
+                expect(wikiElements[i]).not.toBe(undefined);
+                expect(wikiElements[i].type).toBe("TABLE");
+            }
+        });
 
     })
 });
