c3e35bb8270218024bddb96d4ad150341af8005d
Somik Raha <somikr@gmail.com>
2014-08-07 18:17:06 -0700

    Added unit tests

---
+++ tests/unit/fitUtilsSpec.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('FitUtils', function () {
+    var fitUtils: FitUtils;
+    beforeEach(function() {
+       fitUtils = new FitUtils();
+    });
+    it("should camel case Class properly", function() {
+        console.log(fitUtils.camelCaseClass("should i buy milk"));
+        expect(fitUtils.camelCaseClass("should i buy milk")).toBe("ShouldIBuyMilk");
+    });
+    it("should camel case properly", function() {
+        console.log(fitUtils.camelCase("cash in wallet"));
+        expect(fitUtils.camelCase("cash in wallet")).toBe("cashInWallet");
+    });
+});
